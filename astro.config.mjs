7cc60401c193cd6e66b466ca074fe6e283fefc9c
Thereallo <thereallo1026@gmail.com>
2024-08-15 10:51:21 -0400

    Fix image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
+  image: {
+    service: { entrypoint: "astro/assets/services/sharp" },
+  },
 });
